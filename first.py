207f2c6d0fe912e8b5d5a4e45cc350c44d75e944
Kristofer <kris@youngers.org>
2023-03-21 11:44:52 -0400

    decided to track these demo scripts

---
+++ first.py
@@ -0,0 +1,16 @@
+
+# this is a comment
+print('hello zipcode')   # huuh?
+
+x = 5
+y = 6
+
+z = x * y
+
+print(z)
+
+if z == 25:
+	print('z')
+else:
+	print('nope')
+
---
+++ firstpipeline.py
@@ -0,0 +1,48 @@
+# firstpipeline.py
+
+import json
+import requests
+
+response = requests.get("https://jsonplaceholder.typicode.com/todos")
+todos = json.loads(response.text)
+
+# Map of userId to number of complete TODOs for that user
+todos_by_user = {}
+
+# Increment complete TODOs count for each user.
+for todo in todos:
+    if todo["completed"]:
+        try:
+            # Increment the existing user's count.
+            todos_by_user[todo["userId"]] += 1
+        except KeyError:
+            # This user has not been seen. Set their count to 1.
+            todos_by_user[todo["userId"]] = 1
+
+# Create a sorted list of (userId, num_complete) tuples.
+top_users = sorted(todos_by_user.items(), key=lambda x: x[1], reverse=True)
+
+# Get the maximum number of complete TODOs.
+max_complete = top_users[0][1]
+
+# Create a list of all users who have completed
+# the maximum number of TODOs.
+users = []
+for user, num_complete in top_users:
+    if num_complete < max_complete:
+        break
+    users.append(str(user))
+
+max_users = " and ".join(users)
+
+# Define a function to filter out completed TODOs 
+# of users with max completed TODOS.
+def keep(todo):
+    is_complete = todo["completed"]
+    has_max_count = str(todo["userId"]) in users
+    return is_complete and has_max_count
+
+# Write filtered TODOs to file.
+with open("firstresult.json", "w") as data_file:
+    filtered_todos = list(filter(keep, todos))
+    json.dump(filtered_todos, data_file, indent=2)
